868aaeabbab7e7edc44207d2f2e17d0b470eada1
Shantell Smithson <perseus.ripple@gmail.com>
2019-02-16 12:54:24 -0500

    added spacex api fetch

---
+++ public/copy.js
@@ -0,0 +1,36 @@
+/*  
+      let nasaImage = document.createElement('img')
+      nasaImage.src = data.url
+      
+      let heroImage = document.querySelector('.hero-image')
+      heroImage.appendChild(nasaImage)
+    ) */
+
+/*
+          $('.selector').slick({
+            dots: true,
+            prevArrow: $('prev'),
+            nextArrow: $('next'),
+            infinite: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+/*
+          .then(data => {
+            document.getElementById('launch-card').textContent = "launch"
+          }) */
+
+// fetch(API_URL)
+// .then (resp => resp.json())
+// .then(img => {
+//   hero_image.image = image
+//   hero_image.render()
+// })
+
+// const url(https://sdg-astro-api.herokuapp.com/api/Nasa/apod)
+
+// const url(https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming)
+/*const API_URL =
+  'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming' */
--- public/main.js
+++ public/main.js
@@ -1,17 +1,6 @@
-// fetch(API_URL)
-// .then (resp => resp.json())
-// .then(img => {
-//   hero_image.image = image
-//   hero_image.render()
-// })
-
-// const url(https://sdg-astro-api.herokuapp.com/api/Nasa/apod)
-
-// const url(https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming)
-
 const main = () => {
   getPicture()
-  // getLaunches()
+  getLaunch()
 }
 
 const getPicture = () => {
@@ -27,14 +16,23 @@ const getPicture = () => {
       document.getElementById('hero-image').style.backgroundImage = `url(${
         data.hdUrl
       })`
-
-      /*  
-      let nasaImage = document.createElement('img')
-      nasaImage.src = data.url
-      
-      let heroImage = document.querySelector('.hero-image')
-      heroImage.appendChild(nasaImage)
-    ) */
     })
 }
+
+const getLaunch = () => {
+  let launch = `https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming`
+
+  fetch(launch)
+    .then(resp => {
+      return resp.json()
+    })
+
+    .then(data => {
+      console.log(data)
+      document.getElementsByClassName('launches-selector').textContent = `url(${
+        data.hdUrl
+      })`
+    })
+}
+
 document.addEventListener('DOMContentLoaded', main)
